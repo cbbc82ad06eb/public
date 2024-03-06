aa1ecec8f68cdfe91b3a1ffe43eefc27fe56ba93
cbbc82ad06eb <161004215+cbbc82ad06eb@users.noreply.github.com>
2024-03-06 09:46:03 -0800

    Update ubuntu.sh

--- init/ubuntu.sh
+++ init/ubuntu.sh
@@ -1,15 +1,6 @@
 # enable firewall
 sudo ufw enable
 
-# disable sshd password authentication
-sudo tee /etc/ssh/sshd_config.d/sshd.conf <<eos
-Port 22
-AddressFamily inet
-ListenAddress 0.0.0.0
-PasswordAuthentication no
-eos
-sudo systemctl restart ssh
-
 # disable cups, avahi, dns stub listener
 sudo systemctl stop cups.service cups-browsed.service avahi-daemon.service avahi-daemon.socker
 sudo systemctl disable cups.service cups-browsed.service avahi-daemon.service avahi-daemon.socker
